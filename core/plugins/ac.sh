12f81a4fe7a8af14338dd9f4c8ae28b03c34d45a
yashar-vasegh <yashar_vs@yahoo.com>
2015-10-10 11:54:49 +0330

    correcting bash format

--- core/plugins/ac.sh
+++ core/plugins/ac.sh
@@ -3,4 +3,4 @@ day=$(date +%d)
 month=$(LC_ALL=en_US.utf8 date +%b)
 year=$(date +%Y)
 log_file=/var/log/cron-log/
-echo $(grep '$day/$month/$year' $log_file$1 | wc -l)
+echo $(grep "$month $day" $log_file$1 | wc -l)
